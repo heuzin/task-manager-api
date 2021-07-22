3a06bc05e7fdc4b32c699116089d84944fc6ade6
heuzin <matheusqs.arq@gmail.com>
2021-07-21 22:14:26 -0400

    providing access

--- src/app.js
+++ src/app.js
@@ -17,7 +17,11 @@ app.use(function (req, res, next) {
   )
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+    'Content-Type,Authorization'
+  )
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
